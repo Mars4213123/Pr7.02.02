a4bd836caff7d5dba3f5a67cd540ad43d51cf348
Mars4213123 <your_email@example.com>
2025-12-04 21:48:27 +0500

    Получение печеньки авторизации. Кантуганов

--- HttpNewsPAT_Kantuganov/Program.cs
+++ HttpNewsPAT_Kantuganov/Program.cs
@@ -1,26 +1,54 @@
 ﻿
 
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Text;
 
 namespace HttpNewsPAT_Kantuganov
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            WebRequest request = WebRequest.Create("https://59.ru/");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
-            Stream dataStream = response.GetResponseStream();
-            StreamReader reader = new StreamReader(dataStream);
-            string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
-            reader.Close();
-            dataStream.Close();
-            response.Close();
+            SingIn("emilys", "emilyspass");
             Console.Read();
+
+        }
+        public static void SingIn(string username, string password)
+        {
+            CookieContainer cookieContainer = new CookieContainer();
+            string url = "https://dummyjson.com/auth/login";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = cookieContainer;
+
+            string postData = $"username={username}&password={password}";
+            byte[] data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = data.Length;
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            using (HttpWebResponse responseCookieGeter = (HttpWebResponse)request.GetResponse()) {
+                Debug.WriteLine($"Статус выполнения: {responseCookieGeter.StatusCode}");
+                string cookieHeader = responseCookieGeter.Headers["Set-Cookie"]; ;
+
+                Uri uri = new Uri(url);
+                CookieCollection cookies = cookieContainer.GetCookies(uri);
+
+                Console.WriteLine("COOKIES: ");
+                foreach (Cookie cookie in cookies)
+                {
+                    Console.WriteLine($"{cookie.Name}: \n{cookie.Value}");
+                }
+            }
 
         }
     }
